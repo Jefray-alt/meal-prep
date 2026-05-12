184498eb20fa268058cd5bc3d8417d2ff0a6ea2f
Jeffrey France <jbfrance1626@gmail.com>
2026-05-12 21:40:30 +0800

    init: initialize application

---
+++ README.md
@@ -0,0 +1,3 @@
+# Meal Prep
+
+An app that generate meal preps and create a shopping list for it
---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import perfectionist from 'eslint-plugin-perfectionist'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.strictTypeChecked,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+      perfectionist.configs['recommended-natural'],
+    ],
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+])
